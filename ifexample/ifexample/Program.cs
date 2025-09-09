1812f9d6bc152444f203fcc7b936dc04ceb137bc
Gerardo <gerardo.tejada@utp.ac.pa>
2025-09-09 09:19:50 -0500

    nueva rama para los ejercios 14 a 16

---
+++ ifexample/ifexample/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int totalJugador = 24;
+string message = "";
+int totalDealer = 15;
+// Blackjack, Juntar 21 pidiendo cartas o
+// caso de tener menos de 21 tener mayor puntuacion
+// que el dealer
+
+if (totalJugador > totalDealer && totalJugador < 22)
+{
+    message = "Venciste al dealer, felicidades";
+} 
+else if (totalDealer  >= totalJugador) 
+{
+    message = "Perdiste vs el dealer, lo siento";
+}
+else
+{
+    message = "Condicion no valido"; // Condicion invalida cuando el jugador tiene mayor que 22
+}
+    Console.WriteLine(message);
