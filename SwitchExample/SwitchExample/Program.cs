0da0b0e3daa0a981ce2e2ae54ba78ad42dda7956
Gerardo <gerardo.tejada@utp.ac.pa>
2025-09-09 09:40:10 -0500

    Agregando comentarios

--- SwitchExample/SwitchExample/Program.cs
+++ SwitchExample/SwitchExample/Program.cs
@@ -9,27 +9,29 @@ string control = "";
 // que el dealer
 
 Console.WriteLine("Bienvenido al Casino");
-Console.WriteLine("Escriba '21' para jugar al 21");
+Console.WriteLine("Escriba '21' para jugar al 21"); // Pide el string 21 para comenzar
 control = Console.ReadLine();
 
+// Proceso
 switch (control)
 {
     case "21":
         Console.WriteLine("Escriba un numero: ");
-        totalJugador = Convert.ToInt32(Console.ReadLine());
+        totalJugador = Convert.ToInt32(Console.ReadLine()); // Pide un numero 
 
-        if (totalJugador > totalDealer && totalJugador < 22)
+        if (totalJugador > totalDealer && totalJugador < 22) // Si el numero es mayor que el dealer pero menor que 22 
         {
             message = "Venciste al dealer, felicidades";
         }
-        else if (totalDealer >= totalJugador)
+        else if (totalDealer >= totalJugador) // Si el dealer su valores son mayores que el del jugador 
         {
-            message = "Perdiste vs el dealer, lo siento";
+            message = "Perdiste vs el dealer, lo siento"; 
         }
         else
         {
             message = "Condicion no valido"; // Condicion invalida cuando el jugador tiene mayor que 22
         }
+        // Salida valida o invalida
         Console.WriteLine(message);
         break;
     default:
